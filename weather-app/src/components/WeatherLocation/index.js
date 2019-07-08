139bffdd4bb92e9df47b28239e5809fc22e1a89a
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-07-08 10:18:36 -0300

    Se separo la funcionalidad de obtener la temperatura del index para adaptarse al patron SOLID

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,84 +1,54 @@
-import React, {Component} from 'react';
-import convert from 'convert-units';
-import Location from './Location';
-import WeatherData from './WeatherData';
-import './styles.css';
-import {
-    SUN,
-} from './../../constants/weathers';
-
-const location = "Provincia de Córdoba, AR";
-const apiKey = "0d397debf0a0ba538b12007efed4da21";
-const urlBaseWeather = "http://api.openweathermap.org/data/2.5/weather";
-
-const apiWeather = `${urlBaseWeather}?q=${location}&appid=${apiKey}`;
-
+import React, { Component } from "react";
+import transformWeather from "./../../services/transformWeather";
+import { apiWeather } from "./../../constants/apiUrls";
+import Location from "./Location";
+import WeatherData from "./WeatherData";
+import "./styles.css";
+import { SUN } from "./../../constants/weathers";
 
 const data = {
-    temperature: 10,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s',
-}
+  temperature: 10,
+  weatherState: SUN,
+  humidity: 10,
+  wind: "10 m/s"
+};
 
 class WeatherLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "Cordoba",
+      data: data
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            city: "Cordoba",
-            data: data,
-        };
-    }
+  handleUpdateClick = () => {
+    console.log("handleUpdateClick");
 
-    getTemp = kelvin => {
-        return Number(convert(kelvin).from("K").to("C").toFixed(2));
-    }
-
-    getWeatherState = weatherData => {
-        return SUN;
-    }
-
-    getData = weatherData => {
-        const {humidity, temp} = weatherData.main;
-        const {speed} = weatherData.wind;
-        const weatherState = this.getWeatherState(weatherData.weather);
-        const temperature = this.getTemp(temp);
-        const data = {
-            humidity,
-            temperature,
-            weatherState,
-            wind: `${speed} m/s`,
-        }
-        
-        return data;
-    }
-
-    handleUpdateClick = () => {
-        console.log("actualizado");
-
-        fetch(apiWeather).then(resolve => {
-            return resolve.json();
-        }).then(data => {
-            const newWeather = this.getData(data);
-            console.log(newWeather);
-            debugger;
-            this.setState({
-                data: newWeather,
-            });
+    fetch(apiWeather)
+      .then(resolve => {
+        return resolve.json();
+      })
+      .then(data => {
+        const newWeather = transformWeather(data);
+        console.log(newWeather);
+        debugger;
+        this.setState({
+          data: newWeather
         });
-    }
+      });
+  };
 
-    render () {
-        const {city,data} = this.state;
-        return (
-            <div className="weatherLocationCont">
-                <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
-            </div>
-        );
-    }
+  render() {
+    const { city, data } = this.state;
+    return (
+      <div className="weatherLocationCont">
+        <Location city={city} />
+        <WeatherData data={data} />
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
+      </div>
+    );
+  }
 }
 
 export default WeatherLocation;
