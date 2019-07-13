9801b52c5167f518905d7e830f34ce8a9dbe1427
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-07-13 16:52:45 -0300

    Added component ForecastExtended and passed as parameter the selected city in locationList

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,7 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { Grid, Col, Row } from 'react-flexbox-grid';
 import './App.css';
 import LocationList from './components/LocationList';
-
+import ForecastExtended from './components/ForecastExtended';
 
 const cities = [
   'Cordoba, ar',
@@ -16,12 +16,23 @@ const cities = [
   'Lima, pe',
 ];
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      city: null,
+    }
+  }
+
   handleSelectionLocation = city => {
+    this.setState({ city });
     console.log(`handleSelectionLocation ${city}`);
 
   };
 
   render() {
+    const { city } = this.state;
     return (
       <Grid>
         <Row>
@@ -39,7 +50,13 @@ class App extends Component {
           </Col>
           <Col xs={12} md={6}>
             <Paper elevation={4}>
-              <div className="details" />
+              <div className="details">
+                {
+                  !city ?
+                    <h1>No se seleccionó ciudad</h1> :
+                    <ForecastExtended city={city}></ForecastExtended>
+                }
+              </div>
             </Paper>
           </Col>
         </Row>
---
+++ weather-app/src/components/ForecastExtended.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+class ForecastExtended extends Component {
+    render() {
+        const { city } = this.props;
+        return (
+            <div>
+                pronostico extendido para {city}
+            </div>
+        );
+    }
+}
+
+ForecastExtended.propTypes = {
+    city: PropTypes.string.isRequired,
+}
+
+export default ForecastExtended;
