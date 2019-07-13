8699c25e6dd578f3d9dba808e9ab4fd56d6b5a95
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-07-13 18:00:23 -0300

    Added WeatherData component to ForecastItem

--- weather-app/src/components/ForecastExtended.js
+++ weather-app/src/components/ForecastExtended.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
 import './styles.css';
 
+const days = [
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+]
+
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'normal',
+}
+
 class ForecastExtended extends Component {
+
+    renderForecasstItemDays() {
+        return days.map(day => <ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>);
+    }
+
     render() {
         const { city } = this.props;
         return (
             <div>
                 <h2 className='forecast-title'>Pronóstico extendido para {city}</h2>
+                {this.renderForecasstItemDays()}
             </div>
         );
     }
