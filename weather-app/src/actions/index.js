527696f73578c1cb0b6f4bf51da136709a183505
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-07-22 21:09:02 -0300

    129 - Incorporacion de middleware thunk

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,2 +1,23 @@
 export const SET_CITY = 'SET_CITY';
 export const setcity = (payload) => ({ type: SET_CITY, payload });
+
+const apiKey = "0d397debf0a0ba538b12007efed4da21";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
+export const fetchForecast = payload => {
+    return dispatch => {
+        const urlForecast = `${url}?q=${city}&appid=${apiKey}`;
+
+        //activar en el estado una busqueda de datos
+        fetch(urlForecast).then(
+            data => (data.json())
+        ).then(
+            weatherData => {
+                const forecastData = transformForecast(weatherData);
+                this.setState({ forecastData })
+
+                //modificar el estado con el resltado de la promise (fetch)
+            }
+        );
+    }
+};
--- weather-app/src/store/index.js
+++ weather-app/src/store/index.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux';
-import { city } from './../reducers/city'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { city } from './../reducers/city';
+
+import thunk from 'redux-thunk';
 
 const initialState = {
     city: 'Cordoba,ar',
 };
 
-export const store = createStore(city, initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(city, initialState, composeEnhancers(applyMiddleware(thunk)));
