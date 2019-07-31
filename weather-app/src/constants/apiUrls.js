6478157af82782684a4d8d2f59f8e6fa631963b0
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-07-30 21:30:51 -0300

    142 - Creacion de Reducer

--- weather-app/src/constants/apiUrls.js
+++ weather-app/src/constants/apiUrls.js
@@ -1,2 +1,3 @@
 export const apiKey = "0d397debf0a0ba538b12007efed4da21";
 export const urlBaseWeather = "http://api.openweathermap.org/data/2.5/weather";
+export const urlForecast = "http://api.openweathermap.org/data/2.5/forecast";
